ffb5f696370152b575dcd275dfe46b8568e07106
kingluo <home_king@163.com>
2023-09-01 10:35:58 +0800

    add sleep 1 in ADMIN

--- t/http3/common.sh
+++ t/http3/common.sh
@@ -8,6 +8,7 @@ ADMIN() {
     shift 2;
     curl ${ADMIN_SCHEME:-http}://${ADMIN_IP:-127.0.0.1}:${ADMIN_PORT:-9180}/apisix/admin${resource} \
         -H 'X-API-KEY: edd1c9f034335f136f87ad84b625c8f1' -X $method "$@"
+    sleep 1
 }
 
 GREP() {
